04376731ca33cce1b0edf959f8079bfe6dbdffa2
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-08-02 19:38:00 -0300

    criado o listar categorias e usuários aula 74

--- backend-mysql/src/routes.ts
+++ backend-mysql/src/routes.ts
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController'
+import { ListUserController } from './controllers/user/ListUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
 import { DetailUserController } from './controllers/user/DetailUserController'
 import { isAuthenticated } from './middlewares/isAuthenticated'
 
 import { CreateCategoryController } from './controllers/category/CreateCategoryController'
+import { ListCategoryController } from './controllers/category/ListCategoryController'
 
 const router = Router();
 
@@ -13,8 +15,10 @@ const router = Router();
 router.post('/users', new CreateUserController().handle) // rota de cadastro
 router.post('/session', new AuthUserController().handle) // rota de validação
 router.get('/me', isAuthenticated, new DetailUserController().handle) // rota de detalhe
+router.get('/users', isAuthenticated, new ListUserController().handle)
 
 //-- Rotas cateogory -- //
 router.post('/category', isAuthenticated, new CreateCategoryController().handle)
+router.get('/category', isAuthenticated, new ListCategoryController().handle)
 
 export { router };
