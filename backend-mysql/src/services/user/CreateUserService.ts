058e3261ce0d1facc8b2aa07a64981920d4587f6
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-08-02 16:13:13 -0300

    api inserindo com sucesso users aula 67

--- backend-mysql/src/services/user/CreateUserService.ts
+++ backend-mysql/src/services/user/CreateUserService.ts
@@ -1,3 +1,5 @@
+import prismaClient from '../../prisma'
+
 interface UserRequest{
     name: string;
     email: string;
@@ -5,8 +7,35 @@ interface UserRequest{
 }
 class CreateUserService{
     async execute({ name, email, password }: UserRequest) {
-        console.log(name)
-        return { name: name }
+        // console.log(name)
+
+        //Verifica se o email está preenchido ou valido
+        if (!email) {
+            throw new Error('Email é obrigatório')
+        }
+
+        //Verificar se já existe no banco
+        const userAlreadyExists = await prismaClient.user.findFirst({
+            where: {
+                email: email
+            }
+        })
+        if (userAlreadyExists) {
+            throw new Error('Esse email já está cadastrado')
+        }
+        const user = await prismaClient.user.create({
+            data: {
+                name: name,
+                email: email,
+                password: password
+            },
+            select: {
+                id: true,
+                name: true,
+                email: true,
+            }
+        })
+        return { user}
     }
 }
 
