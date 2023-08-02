4c642375626541e9a502ee1b40d1f068d0f7b3f9
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-08-02 18:30:07 -0300

    aula 71 finalizada regra de middware validando token

--- backend-mysql/src/routes.ts
+++ backend-mysql/src/routes.ts
@@ -1,13 +1,16 @@
-import { Router, Request, Response } from 'express';
+import { Router } from 'express';
 
 import { CreateUserController } from './controllers/user/CreateUserController'
 import { AuthUserController } from './controllers/user/AuthUserController'
+import { DetailUserController } from './controllers/user/DetailUserController'
+import { isAuthenticated } from './middlewares/isAuthenticated'
 
 const router = Router();
 
 //Rotas criadas
-router.post('/users', new CreateUserController().handle)
-router.post('/session', new AuthUserController().handle)
+router.post('/users', new CreateUserController().handle) // rota de cadastro
+router.post('/session', new AuthUserController().handle) // rota de validação
+router.get('/me', isAuthenticated, new DetailUserController().handle) // rota de detalhe
 
 // router.get('/users', new CreateUserController().handle)
 
