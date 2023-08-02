9a3f09b24f2127a83a25d59e14a576fad0943418
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-08-02 17:38:06 -0300

    aula 70 finalizada jwt configurado com sucesso

--- backend-mysql/src/services/user/AuthUserService.ts
+++ backend-mysql/src/services/user/AuthUserService.ts
@@ -1,5 +1,6 @@
 import prismaClient from "../../prisma"
 import { compare } from "bcryptjs";
+import { sign } from "jsonwebtoken"
 
 interface Authrequest{
     email: string;
@@ -12,27 +13,44 @@ class AuthUserService{
         // return {  status: result }
 
         // Verificar se o email existe
-        const userEmail = await prismaClient.user.findFirst({
+        const user = await prismaClient.user.findFirst({
             where: {
                 email: email
             }
         })
 
-        if (!userEmail) {
+        if (!user) {
             throw new Error("Email não encontrado")
         }
 
         // Verificar se a senha está correta
-        const passwordMath = await compare(password, userEmail.password)
+        const passwordMath = await compare(password, user.password)
 
         if (!passwordMath) {
             throw new Error("Senha incorreta!")
         }
 
-        return { ok: true }
-        // gerar o token JWT 
+        // return { ok: true }
+        // gerar o token JWT
 
-        
+        const token = sign(
+            {
+                name: user.name,
+                email: user.email
+            },
+            process.env.JWT_SECRET!, // expreimenta tirar o ponto de exclamação!
+            {
+                subject: user.id,
+                expiresIn: '30d'
+            }
+        )
+
+        return {
+            id: user.id,
+            name: user.name,
+            email: user.email,
+            token: token
+        }
     }
 }
 
