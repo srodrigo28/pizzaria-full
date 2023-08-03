9f0687ae92bd99652f13fdcd42dc2808c90f016f
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-08-03 12:53:57 -0300

    aula 84 alterando order item finalizada

--- backend-mysql/src/routes.ts
+++ backend-mysql/src/routes.ts
@@ -15,6 +15,9 @@ import { ListByCategoryController } from './controllers/product/ListByCategoryCo
 
 import { CreateOrderController } from './controllers/order/CreateOrderController'
 import { RemoveOrderController } from './controllers/order/RemoveOrderController'
+import { AddItemController } from './controllers/order/AddItemController'
+import { RemoveItemController } from './controllers/order/RemoveItemController'
+import { SendOrderController } from './controllers/order/SendOrderController'
 
 import uploadConfig from './config/multer'
 
@@ -37,6 +40,9 @@ router.post('/product', isAuthenticated, upload.single('file'), new CreateProduc
 
 //-- Rotas Order -- //
 router.post('/order', isAuthenticated, new CreateOrderController().handle)
+router.post('/order/add', isAuthenticated, new AddItemController().handle)
 router.delete('/order', isAuthenticated, new RemoveOrderController().handle)
+router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
 
 export { router };
