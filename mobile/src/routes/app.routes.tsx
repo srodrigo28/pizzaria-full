4630d9c633d179013e08448c632df6daecba5ed0
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-08-16 20:53:03 -0300

    projeto limpo para sobre escrever

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,17 +1,1 @@
-import React from 'react';
-
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-
-import Dashboard from '../pages/Dashboard';
-
-const Stack = createNativeStackNavigator();
-
-function AppRoutes(){
-    return(
-        <Stack.Navigator>
-            <Stack.Screen name='Dashboard' component={Dashboard} />
-        </Stack.Navigator>
-    )
-}
-
-export default AppRoutes;
+// 
