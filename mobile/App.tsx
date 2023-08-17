d2d1cd144d7d011225c05c54480345225524fe66
Sebastião Rodrigo <rodrigoexer1@gmail.com>
2023-08-16 22:29:03 -0300

    updated

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,1 +1,27 @@
-// 
+import { StatusBar, StyleSheet } from 'react-native'
+import { NavigationContainer } from '@react-navigation/native'
+import Routes from './src/routes';
+
+export default function App(){
+  <NavigationContainer>
+    <StatusBar 
+      translucent={false}
+      barStyle="light-content" 
+      backgroundColor="#1d1d2e" 
+    />
+    <Routes />
+  </NavigationContainer>
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  titulo:{
+    color: 'red',
+    fontSize: 40
+  }
+});
---
+++ mobile/readme.md
@@ -0,0 +1,32 @@
+### principais comandos usados
+* iniciar o expo
+```
+npx expo start
+```
+### Dependências navegação
+* Link para sub links
+```
+ https://reactnavigation.org/docs/getting-started/
+```
+* Navigation
+```
+npm install @react-navigation/native
+```
+
+* Native Safe Area
+```
+npx expo install react-native-screens react-native-safe-area-context    
+```
+
+* Stack tipo de navegação
+```
+npm install @react-navigation/stack
+```
+
+### Outras dependências
+* Axios
+```
+```
+* LocalStorage
+```
+```
